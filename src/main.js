a9c28873c971151f0d725c4149cd3b851e5497b1
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-12-22 19:37:15 +0100

    feat: initialize firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router/index'
 
-createApp(App).mount('#app')
+/* Firebase */
+import { initializeApp } from 'firebase/app'
+
+// Initialize the Firebase app
+initializeApp({
+  apiKey: 'AIzaSyDOmnkk-r_DFtikCTyHrMXxpSfv7z9K5ws',
+  authDomain: 'campus-app-521d0.firebaseapp.com',
+  projectId: 'campus-app-521d0',
+  storageBucket: 'campus-app-521d0.appspot.com',
+  messagingSenderId: '66703311140',
+  appId: '1:66703311140:web:378705cef1f7d0996733b1'
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
