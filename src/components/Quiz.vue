d8a5d4afd947f0cf47b1c12fbf9ca70b708030b3
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-12-22 19:38:21 +0100

    feat: add Quiz component

---
+++ src/components/Quiz.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span>
+      <button>+</button>
+    </span>
+    <div class="grid">
+      <div class="grid-item">Question</div>
+      <div class="grid-item">Something</div>
+      <div class="grid-item">Another thing</div>
+      <div class="grid-item">What?</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz',
+  setup() {
+    console.log('hi')
+  }
+}
+</script>
+
+
+<style scoped>
+div.grid {
+  margin-top: 1rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+}
+
+div.grid-item {
+  border: 1px solid red;
+  padding: 20%;
+}
+</style>
