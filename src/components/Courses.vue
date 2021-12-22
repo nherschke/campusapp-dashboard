696d0a5510c8967ed5355cc96b22e71cdb97e72c
Nicolas Herschke <nicolas.herschke@gmx.de>
2021-12-22 19:38:04 +0100

    feat: add Courses component and basic course view

---
+++ src/components/Courses.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>Meine Kurse</h1>
+  <div class="grid">
+    <div class="grid-item" v-for="course in courses" :key="course.id">
+      <h2>{{ course.name }}</h2>
+      <h3>{{ course.room }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+export default {
+  setup() {
+    const courses = ref([])
+
+    const q = query(
+      collection(getFirestore(), 'courses'),
+      where('profId', '==', getAuth().currentUser.uid)
+    )
+    getDocs(q).then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        courses.value.push({ id: doc.id, ...doc.data() })
+      })
+    })
+
+    return {
+      courses
+    }
+  }
+}
+</script>
+
+<style>
+div.grid {
+  margin-top: 1rem;
+  padding: 0 100px;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+}
+
+div.grid-item {
+  color: #fff;
+  background: #fa8128;
+  border-radius: 7px;
+  padding: 10px 0;
+}
+</style>
